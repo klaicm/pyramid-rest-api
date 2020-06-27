35c9548b16b833c52e414ee6cd290d36d66650eb
klaicm <martin.klaic@gmail.com>
2020-06-27 10:16:38 +0200

    New old csrf config. Changed table name. Changed data.sql

--- src/main/java/klaicm/backlayer/tennisscores/model/DAOUser.java
+++ src/main/java/klaicm/backlayer/tennisscores/model/DAOUser.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_account")
 public class DAOUser extends BaseEntity {
 
     @Column
